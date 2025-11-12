d20d07e62009bb6ecde57f7a5486100b8aac8fb0
Doru <131837270+P4rthicular@users.noreply.github.com>
2025-11-12 21:59:15 +0800

    30% update
    - better label and ui layout
    - started disasters coding
    - started project and initiative panels

--- levels/MainScript.gd
+++ levels/MainScript.gd
@@ -1,5 +1,7 @@
 extends Control
-
+# ------------------------------------------- #
+# --------- CORE GAMEPLAY SECTION ----------- #
+# ------------------------------------------- #
 # Current Day Number
 var day = 0
 
@@ -10,7 +12,6 @@ var happiness = 20
 var security = 20
 var awareness = 20
 var income = 0
-
 # How much Income you should get every day
 #This exists so the game can change how much Income you earn
 # Should also be dependent on difficulty
@@ -23,8 +24,8 @@ var incomerate = 3
 @export var resc3: Label
 @export var resc4: Label
 
-# List of stuff that should be displayed in the event box
-var eventtext = [
+# Test stuff to make sure the displaybox works
+var dailytext = [
 	"Test 1. YAY YIPPE WAHHOOO",
 	"Test 2. WOAH WOWW WOOOOOO",
 	"Test 3. YWOAOWOAWOAWOAWOAWO",
@@ -45,37 +46,33 @@ var eventtext = [
 # Anything in here is run as soon as the game loads
 # Needs to detect what level or difficulty is chosen
 func _ready():
+	randomize()
 	NewDay()
+
 
 func NewDay():
 	# This runs everytime a new day is generated, either by ending turn or starting a new game
 	day += 1
 	print (day)
-	$daylbl.text = "Day " + str(day)
+	$resources/VBoxContainer2/daylbl.text = "Day " + str(day)
 	resource_update()
-	$incomelbl.text = "Income: $" + str(income)
-	$eventbox.text += "\n[Day " + str(day) + "]: " + eventtext.pick_random() + "\n" # This should have random text relating to the daily event how do I do this
+	income += incomerate
+	$resources/VBoxContainer2/incomelbl.text = "Income: $" + str(income)
+	disaster_roll()
+	disastertime -= 1
+	$eventbox.text += "\n[Day " + str(day) + "]: " + dailytext.pick_random() + "\n" # This should have random text relating to the daily event how do I do this
 	$eventbtn.disabled = false
 	
 
 func resource_update():
 	# This updates the player's resource stats
-	# Sets resource to 0 if it's a negative integer
-	# Maybe theres a better way idk for now
-	if awareness < 0:
-		awareness = 0
-	if happiness < 0:
-		happiness = 0
-	if security < 0:
-		security = 0
-	if pollution > 100:
-		pollution = 100
-	# For some reason this is the only working solution
+	
+	# Updates the resource labels
 	resc1.text = "Pollution: " + str(pollution)
 	resc2.text = "Happiness: " + str(happiness)
 	resc3.text = "Security: " + str(security)
 	resc4.text = "Awareness: " + str(awareness)
-	income += incomerate # THIS DOESNT WORK FOR NOW WHY
+	$resources/VBoxContainer2/incomelbl.text = "Income: $" + str(income)
 
 
 func quit_to_menu() -> void:
@@ -88,32 +85,87 @@ func nextday_pressed() -> void:
 	# NEEDS CONFIRMATION
 	# This one generates the next day
 	NewDay()
-	
 
+# ------------------------------------ #
+# --------- EVENTS SECTION ----------- #
+# ------------------------------------ #
 
+# Dedicded to try keeping all the everything in one script hope it works well
+# Events section where the game decides what disaster
+
+# This identifies what disaster is going to happen
+var disaster = null
+var disastertime = 0 # How much turns you have until the disaster arrives
+var disasterlist = [ # List of available disasters, will add more soon
+	"earthquake",
+	"supertyphoon",
+	"tsunami",
+	"tornado",
+	"flashflood"
+]
+
+# This function chooses which disaster will happen on your game
+func disaster_roll() -> void:
+	print ("disaster roll check")
+	if disaster == null: # First checks if you already have a disaster
+		print ("disaster null check")
+		if day < 5 and day > 1 or day == 5: # Second checks what day it is to make sure disaster is chosen on or before day 5
+			print ("disaster day check")
+			var disasterchance = randf()
+			if disasterchance >= 0.33: # 33 percent chance on day 2 to 4 for the disaster to be chosen
+				disaster = disasterlist.pick_random()
+				print (disaster)
+				$resources/VBoxContainer2/disasterlbl.text = "Current Disaster: " + str(disaster) # This should be hidden from the player and events will reveal what disaster it is
+				print ("current disaster chosen as ", disaster)
+				disastertime += randi_range(34, 52) # Determines how many turns before the disaster appears
+				print ("disaster deadline is ", disastertime, " turns")
+			else:
+				pass
+	else:
+		print ("disaster already found")
+		pass
+
+# This is for testing purposes this should be removed when gameplay loop is complete
 func eventbtn_pressed() -> void:
 	if income >= 4:
+		income -= 4
 		var eventchance = randf()
-		if eventchance <= 0.75:
+		if eventchance <= 0.60:
 			$eventbox.text += "\n[Day " + str(day) + "]: Negative event. Lose stats. I dislike you. Money left: " + str(income) + "\n"
 		
 			pollution += randi() % 6
 			happiness -= randi() % 6
 			security -= randi() % 6
 			awareness -= randi() % 6
-			income -= 4
 			resource_update()
 		else:
 			$eventbox.text += "\n[Day " + str(day) + "]: Positive event! Gain Stats! I love you! Money left: " + str(income) + "\n"
 		
-			pollution -= randi() % 5
-			happiness += randi() % 5
-			security += randi() % 5
-			awareness += randi() % 5
-			income -= 4
+			pollution -= randi() % 8
+			happiness += randi() % 8
+			security += randi() % 8
+			awareness += randi() % 8
 			resource_update()
 	else:
 		$eventbox.text += "\n[Day " + str(day) + "]: No money. Poor. Go get more money." + "\n"
 	$eventbtn.disabled = true
+
+
+# ------------------------------------ #
+# --------- PANELS SECTION ----------- #
+# ------------------------------------ #
+
+
+# ------------ PROJECTS -------------- #
+func _show_projects() -> void:
+	$projectpnl.visible = !$projectpnl.visible
 	
-		
+func _close_projects() -> void:
+	$projectpnl.visible = !$projectpnl.visible
+
+# ---------- INITIATIVES ------------- #
+func _show_initiatives() -> void:
+	$initiativepnl.visible = !$initiativepnl.visible
+
+func _close_initiatives() -> void:
+	$initiativepnl.visible = !$initiativepnl.visible
